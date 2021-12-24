8fc9778f5dbc477a7fd2f14b731767a0128f85d0
HJ1X <54548986+HJ1X@users.noreply.github.com>
2021-12-24 16:31:48 +0530

    initialization

---
+++ App.js
@@ -0,0 +1,40 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {ThemeContext} from './app1/app/config/theme-context';
+import {NavigationContainer} from '@react-navigation/native';
+
+import WelcomeScreenStackNav from './app1/app/navigation/WelcomeScreenStackNav';
+import navigationTheme from './app1/app/navigation/navigationTheme';
+
+export default () => {
+  const [theme, setTheme] = React.useState('light');
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <ApplicationProvider {...eva} theme={eva[theme]}>
+          <IconRegistry icons={EvaIconsPack} />
+          <WelcomeScreenStackNav />
+        </ApplicationProvider>
+      </ThemeContext.Provider>
+    </NavigationContainer>
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: {flex: 1},
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
